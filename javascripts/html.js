779db5cfda0309495cf9601c402180dc7bd3372e
bhserna <bhserna@gmail.com>
2015-11-29 19:25:39 -0600

    Automated commit at 2015-11-30 01:25:38 UTC by middleman-deploy 0.3.0

---
+++ javascripts/html.js
@@ -0,0 +1,95 @@
+(function() {
+  var Templates, renderIf, renderMain, renderMany, renderUnless;
+
+  window.Html = {
+    render: function(shop) {
+      return Templates.application(shop);
+    },
+    renderMany: function(collection, template) {
+      var item;
+      return ((function() {
+        var i, len, results;
+        results = [];
+        for (i = 0, len = collection.length; i < len; i++) {
+          item = collection[i];
+          results.push(template(item));
+        }
+        return results;
+      })()).join("");
+    },
+    renderMain: function(shop) {
+      var pageName;
+      pageName = shop.currentPage + "Page";
+      if (!(pageName in Templates)) {
+        throw "NoPageError: Page with name " + pageName + " is not defined";
+      }
+      return Templates[pageName](shop);
+    },
+    renderIf: function(condition, renderable) {
+      if (condition) {
+        return renderable();
+      } else {
+        return "";
+      }
+    },
+    renderUnless: function(condition, renderable) {
+      if (condition) {
+        return "";
+      } else {
+        return renderable();
+      }
+    }
+  };
+
+  window.App = {
+    init: function($el, shop1) {
+      this.$el = $el;
+      this.shop = shop1;
+    },
+    render: function() {
+      return this.$el.html(Html.render(this.shop));
+    },
+    update: function(shop) {
+      this.shop = shop;
+      return this.render();
+    }
+  };
+
+  window.Actions = {
+    onAction: function(type, name, fun) {
+      return $(document).on(type, "[data-action=" + name + "]", function(event) {
+        return fun(App.shop, $(this));
+      });
+    }
+  };
+
+  renderMain = Html.renderMain, renderMany = Html.renderMany, renderIf = Html.renderIf, renderUnless = Html.renderUnless;
+
+  Templates = {
+    application: function(shop) {
+      return "<div class=\"top-bar\">\n  <h1>Granito Divino</h1>\n  <a class=\"top-bar__action\" data-action=\"navigateTo\" data-page=\"capturedOrders\">\n    Ordenes capturadas\n  </a>\n</div>\n\n<ul class=\"navbar\">\n  <li>\n    <a href=\"#\" data-action=\"navigateTo\" data-page=\"products\">\n      Productos\n    </a>\n  </li>\n  <li>\n    <a href=\"#\"\n      " + (shop.currentOrder.isFlashed ? "class='flashy'" : void 0) + "}\n      data-action=\"navigateTo\"\n      data-page=\"currentOrder\">\n      Orden - $" + shop.currentOrder.total + "\n    </a>\n  </li>\n</ul>\n\n<div class=\"content\">\n  " + (renderMain(shop)) + "\n</div>";
+    },
+    productsPage: function(shop) {
+      return "<div class=\"product-list\">\n  " + (renderMany(shop.products, this.productItem)) + "\n</div>";
+    },
+    capturedOrdersPage: function(shop) {
+      return "<ul>\n  " + (renderMany(shop.capturedOrders, this.capturedOrder)) + "\n</ul>";
+    },
+    capturedOrder: function(order) {
+      return "<li>\n  <strong>" + (moment(order.capturedAt).format("ddd, hA")) + ":</strong>\n  <span>$" + order.total + "</strong>\n</li>";
+    },
+    currentOrderPage: function(shop) {
+      return "<div class=\"order\">\n  <table>\n    <thead>\n      <tr>\n        <th>Producto</th>\n        <th class=\"order__count-column\">Cantidad</th>\n        <th class=\"order__total-column\">Total</th>\n      </tr>\n    </thead>\n    <tbody>\n      " + (renderMany(shop.currentOrder.items, this.orderItem)) + "\n\n      <tr>\n        <td></td>\n        <td class=\"order__count-column\"><strong>Total:</strong></td>\n        <td class=\"order__total-column " + (shop.currentOrder.isFlashed ? "flashy" : void 0) + "\">\n          $" + shop.currentOrder.total + "\n        </td>\n      </tr>\n\n      " + (renderIf(shop.currentOrder.canBeCaptured, this.orderButtons)) + "\n    </tbody>\n  </table>\n</div>";
+    },
+    orderButtons: function() {
+      return "<tr>\n  <td colspan=\"3\" class=\"text-center\">\n    <div class=\"order__buttons\">\n      <button class=\"order__cancel-button\" data-action=\"cancelOrder\">\n        Cancelar\n      </button>\n      <button data-action=\"captureOrder\">\n        Capturar\n      </button>\n    </div>\n  </td>\n</tr>";
+    },
+    orderItem: function(item) {
+      return "<tr>\n  <td>" + item.name + "</td>\n  <td class=\"order__count-column\">\n    <button data-action=\"decrementCount\" data-id=\"" + item.productId + "\">\n      <span class=\"fa fa-minus\"></span>\n    </button>\n    <span class=\"order__item-count\">" + item.count + "</span>\n    <button data-action=\"incrementCount\" data-id=\"" + item.productId + "\">\n      <span class=\"fa fa-plus\"></span>\n    </button>\n  </td>\n  <td class=\"order__total-column\">$" + item.total + "</td>\n</tr>";
+    },
+    productItem: function(product) {
+      return "<div class=\"product-item\" data-action=\"addItem\" data-id=" + product.id + ">\n  <div class=\"product-item__body\">\n    <h1>" + product.name + "</h1>\n    <span>$" + product.price + "</span>\n  </div>\n</div>";
+    }
+  };
+
+}).call(this);
---
+++ javascripts/shop.js
@@ -0,0 +1,290 @@
+(function() {
+  var Dict, Item, LocalStore, Order, Products, loadState, onAction, shopProducts, updateState;
+
+  Dict = {
+    update: function(object, props) {
+      return _.extend({}, object, props);
+    }
+  };
+
+  window.Time = {
+    now: function() {
+      return moment().format();
+    },
+    utc: function() {
+      return moment.utc().format();
+    }
+  };
+
+  LocalStore = {
+    save: function(state) {
+      return localStorage.shop = JSON.stringify(state);
+    },
+    load: function() {
+      return JSON.parse(localStorage.shop || "{}");
+    }
+  };
+
+  Item = {
+    haveSameProduct: function(item1, item2) {
+      return item1.productId === item2.productId;
+    },
+    newFromProduct: function(product) {
+      return {
+        productId: product.id,
+        name: product.name,
+        count: 1,
+        total: product.price,
+        price: product.price
+      };
+    },
+    incrementCount: function(item) {
+      var count;
+      count = item.count + 1;
+      return this.update(item, {
+        count: count,
+        total: item.price * count
+      });
+    },
+    decrementCount: function(item) {
+      var count;
+      count = item.count - 1;
+      return this.update(item, {
+        count: count,
+        total: item.price * count
+      });
+    },
+    sort: function(items) {
+      return _.sortBy(items, "productId");
+    },
+    update: function(item, props) {
+      return Dict.update(item, props);
+    }
+  };
+
+  Products = {
+    findWithId: function(products, id) {
+      var i, len, product;
+      for (i = 0, len = products.length; i < len; i++) {
+        product = products[i];
+        if (product.id === id) {
+          return product;
+        }
+      }
+    }
+  };
+
+  Order = {
+    "new": function(items, options) {
+      if (items == null) {
+        items = [];
+      }
+      if (options == null) {
+        options = {};
+      }
+      return {
+        items: Item.sort(items),
+        total: _.reduce(items, (function(acc, item) {
+          return acc + item.total;
+        }), 0),
+        canBeCaptured: _.some(items),
+        isFlashed: options.isFlashed || false,
+        capturedAt: options.capturedAt || null
+      };
+    },
+    flashTotal: function(order) {
+      return this["new"](order.items, {
+        isFlashed: true
+      });
+    },
+    resetFlashes: function(order) {
+      return this["new"](order.items, {
+        isFlashed: false
+      });
+    },
+    findItem: function(order, productId) {
+      return _.findWhere(order.items, {
+        productId: productId
+      });
+    },
+    insertItem: function(order, itemToInsert) {
+      var items;
+      items = _.filter(order.items, function(item) {
+        return !Item.haveSameProduct(itemToInsert, item);
+      });
+      return this["new"](items.concat(itemToInsert));
+    },
+    removeItem: function(order, itemToInsert) {
+      var items;
+      items = _.filter(order.items, function(item) {
+        return !Item.haveSameProduct(itemToInsert, item);
+      });
+      return this["new"](items);
+    },
+    addItem: function(order, product) {
+      var item;
+      item = this.findItem(order, product.id);
+      item = item ? Item.incrementCount(item) : Item.newFromProduct(product);
+      return this.insertItem(order, item);
+    },
+    incrementCount: function(order, product) {
+      var item;
+      item = this.findItem(order, product.id);
+      item = Item.incrementCount(item);
+      return this.insertItem(order, item);
+    },
+    decrementCount: function(order, product) {
+      var item;
+      item = this.findItem(order, product.id);
+      item = Item.decrementCount(item);
+      if (item.count === 0) {
+        return this.removeItem(order, item);
+      } else {
+        return this.insertItem(order, item);
+      }
+    },
+    capture: function(order) {
+      return this["new"](order.items, {
+        capturedAt: Time.now()
+      });
+    }
+  };
+
+  window.Shop = {
+    "new": function(products, order, currentPage) {
+      return {
+        products: products,
+        currentOrder: order || Order["new"](),
+        capturedOrders: [],
+        currentPage: currentPage || "products"
+      };
+    },
+    changePage: function(shop, page) {
+      return this.update(shop, {
+        currentPage: page
+      });
+    },
+    cancelOrder: function(shop) {
+      return this.updateCurrentOrder(shop, Order["new"]());
+    },
+    addToOrder: function(shop, productId) {
+      var order, product;
+      product = Products.findWithId(shop.products, productId);
+      order = Order.addItem(shop.currentOrder, product);
+      return this.updateCurrentOrder(shop, order);
+    },
+    incrementCountInOrder: function(shop, productId) {
+      var order, product;
+      product = Products.findWithId(shop.products, productId);
+      order = Order.incrementCount(shop.currentOrder, product);
+      return this.updateCurrentOrder(shop, order);
+    },
+    decrementCountInOrder: function(shop, productId) {
+      var order, product;
+      product = Products.findWithId(shop.products, productId);
+      order = Order.decrementCount(shop.currentOrder, product);
+      return this.updateCurrentOrder(shop, order);
+    },
+    captureOrder: function(shop) {
+      var order;
+      order = Order.capture(shop.currentOrder);
+      shop = this.update(shop, {
+        capturedOrders: shop.capturedOrders.concat(order)
+      });
+      return this.updateCurrentOrder(shop, Order["new"]());
+    },
+    updateCurrentOrder: function(shop, order) {
+      return this.update(shop, {
+        currentOrder: Order.flashTotal(order)
+      });
+    },
+    update: function(shop, props) {
+      var order;
+      if (!props.currentOrder) {
+        order = Order.resetFlashes(shop.currentOrder);
+        props = Dict.update(props, {
+          currentOrder: order
+        });
+      }
+      return Dict.update(shop, props);
+    }
+  };
+
+  shopProducts = [
+    {
+      id: "e1",
+      name: "Elote chico",
+      price: 15.00
+    }, {
+      id: "e2",
+      name: "Elote mediano",
+      price: 20.00
+    }, {
+      id: "e3",
+      name: "Elote grande",
+      price: 25.00
+    }, {
+      id: "e4",
+      name: "Conchitas",
+      price: 25.00
+    }, {
+      id: "e5",
+      name: "Tostitos",
+      price: 30.00
+    }
+  ];
+
+  onAction = Actions.onAction;
+
+  onAction("click", "navigateTo", function(shop, $el) {
+    var page;
+    page = $el.data("page");
+    return updateState(Shop.changePage(shop, page));
+  });
+
+  onAction("click", "addItem", function(shop, $el) {
+    var productId;
+    productId = $el.data("id");
+    return updateState(Shop.addToOrder(shop, productId));
+  });
+
+  onAction("click", "incrementCount", function(shop, $el) {
+    var productId;
+    productId = $el.data("id");
+    return updateState(Shop.incrementCountInOrder(shop, productId));
+  });
+
+  onAction("click", "decrementCount", function(shop, $el) {
+    var productId;
+    productId = $el.data("id");
+    return updateState(Shop.decrementCountInOrder(shop, productId));
+  });
+
+  onAction("click", "cancelOrder", function(shop) {
+    return updateState(Shop.cancelOrder(shop));
+  });
+
+  onAction("click", "captureOrder", function(shop) {
+    return updateState(Shop.captureOrder(shop));
+  });
+
+  updateState = function(shop) {
+    App.update(shop);
+    return LocalStore.save(shop);
+  };
+
+  loadState = function() {
+    var shop;
+    if (_.isEmpty(shop = LocalStore.load())) {
+      return Shop["new"](shopProducts);
+    } else {
+      return shop;
+    }
+  };
+
+  $(function() {
+    App.init($(".js-app"), loadState());
+    return App.render();
+  });
+
+}).call(this);
